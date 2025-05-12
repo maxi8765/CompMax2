1fdf9c4f12c6ed930835af3a5587d53bb7d3aee1
maxi8765 <maxwell.ian@gmail.com>
2025-05-12 04:42:53 +0000

    Deploying to gh-pages from @ maxi8765/CompMax2@f64c3d57cb43a81e4cf172de8b5cce1cf7608449 🚀

--- js/config.js
+++ js/config.js
@@ -9,9 +9,9 @@ const CONFIG = {
     // EmailJS configuration
     emailjs: {
         // Public key for EmailJS - should be replaced during build process
-        publicKey: "YOUR_PUBLIC_KEY", // Change this
-        serviceId: "YOUR_SERVICE_ID", // Change this
-        templateId: "YOUR_TEMPLATE_ID" // Change this
+        publicKey: "aC33MyvXhim_7HkU6", 
+        serviceId: "service_0zbtwjq", 
+        templateId: "template_vd3qwz8"
     },
     
     // Application defaults
